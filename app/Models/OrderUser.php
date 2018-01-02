d8b1aa6813adf7171b88d39aba2ccdd626b24773
fellipegpbotelho <fellipegpbotelho@gmail.com>
2018-01-02 10:36:23 -0200

    Criação dos controller de admin, model OrderUser

---
+++ app/Models/OrderUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderUser extends Model{
+
+    protected $table = 'order_user';
+    protected $fillable = ['user_id', 'product_id', 'status'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,9 +9,16 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/logout', 'Auth\LoginController@logoutUser')->name('user.logout');
 
+// Auth Admin
 Route::prefix('admin')->group(function() {
     Route::get('/', 'AdminController@index')->name('admin.home');
     Route::get('/login', 'AuthAdmin\LoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'AuthAdmin\LoginController@login')->name('admin.login.submit');
     Route::get('/logout', 'AuthAdmin\LoginController@logout')->name('admin.logout');
+});
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function(){
+    Route::resource('users', 'UsersController', ['only' => ['index']]);
+    Route::resource('orders', 'OrdersController', ['only' => ['index']]);
+    Route::resource('products', 'ProductsController', ['only' => ['index']]);
 });
